ca6aef3fd11f3ee95372b79f948beec053e6393a
dayo <dayo@Ubuntu22.10.myguest.virtualbox.org>
2023-02-03 22:15:39 +0100

    fetch fuel data

---
+++ scrape/scrape_fuel.py
@@ -0,0 +1,28 @@
+import json
+
+from libraries.base_scrape import BaseScrape
+from libraries.post_to_api import PostToAPI
+
+class ScrapeFuel(BaseScrape):
+    
+    def scrape(self, country: str):
+        soup = self._read_file()
+        table_rows = soup.find("div", {"id":"graphPageLeft"}).find('tbody').find_all('tr')
+
+        data_dict = {}
+        for row in table_rows:
+            currency = str(row.find('th').text).strip().lower()
+            prices = []
+            for cell in row.find_all('td'):
+                prices.append(cell.text)
+
+            if currency in ['usd', 'eur']:
+                data_dict[f'{currency}_price_per_litre'] = float(str(prices[0]).replace(',', ''))
+                data_dict[f'{currency}_price_per_gallon'] = float(str(prices[1]).replace(',', ''))
+            data_dict['country'] = country
+            # print(data_dict)
+        
+        PostToAPI('http://127.0.0.1:8000/add_fuel/', json.dumps(data_dict)).api_post()
+
+if __name__=="__main__":
+    ScrapeFuel('tmp/nigeria_fuel_price.html').scrape('NG')
